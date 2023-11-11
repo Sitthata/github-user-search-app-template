323b9315c2c6b4b465070b741a343254746af1d6
Sitthata <aprils96950@gmail.com>
2023-11-12 02:27:14 +0700

    Template-Start

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,6 @@ function App() {
     // Fetch user data from GitHub API
   };
 
-  const data = {
-    image: "https://avatars.githubusercontent.com/u/1?v=4",
-    username: "mojombo",
-    createdAt: "2007-10-20T05:24:19Z",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque libero sequi similique beatae asperiores natus dignissimos quia excepturi iste expedita?",
-    publicRepos: 61,
-    followers: 23200,
-    following: 0,
-  };
-
   return (
     <section className="font-mono text-white">
       <div className="flex flex-col gap-10 px-5 mt-5 sm:mx-auto sm:max-w-[40rem]">
@@ -31,7 +21,7 @@ function App() {
             setUsername={setUsername}
             handleClick={fetchUser}
           />
-          <Card data={data} />
+          {/* <Card data={data} /> */}
         </div>
       </div>
     </section>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,13 @@
 import { CardType } from "../types/cardType";
-import Display from "./Display";
-import NotFound from "./NotFound";
 
 interface CardProps {
   data?: CardType | null;
 }
 
 const Card = ({ data }: CardProps) => {
-  if (!data) return <NotFound />;
-  const { image, username, createdAt, bio } = data;
   return (
-    <div className="flex flex-col gap-5 p-5 rounded-md bg-primary-100">
-      <div className="flex gap-4">
-        <img src={image} alt="" className="w-24 h-24 rounded-full" />
-        <div>
-          <h2 className="font-semibold">{username}</h2>
-          <h2>Joined-At: {createdAt}</h2>
-        </div>
-      </div>
-      <p>{bio}</p>
-      <Display />
+    <div>
+      Card
     </div>
   );
 };
