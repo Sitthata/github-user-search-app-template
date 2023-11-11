3577d9657a73344e1a6325fa7ccb8889ec5d9993
Sitthata <aprils96950@gmail.com>
2023-11-12 02:42:47 +0700

    finished dev finder

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,33 @@ import Card from "./components/Card";
 import NavBar from "./components/NavBar";
 import SearchBox from "./components/SearchBox";
 import { useState } from "react";
+import { CardType } from "./types/cardType";
 
 function App() {
   const [username, setUsername] = useState("");
-  // const [data, setData] = useState<CardType | null>(null);
+  const [data, setData] = useState<CardType | null>(null);
 
-  const fetchUser = () => {
-    // Fetch user data from GitHub API
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        // If the response is not ok, throw an error to be caught below
+        throw new Error('User not found');
+      }
+      const jsonData = await response.json();
+      setData({
+        image: jsonData.avatar_url,
+        username: jsonData.login,
+        createdAt: jsonData.created_at,
+        bio: jsonData.bio,
+        publicRepos: jsonData.public_repos,
+        followers: jsonData.followers,
+        following: jsonData.following,
+      });
+    } catch (err) {
+      console.error(err);
+      setData(null); 
+    }
   };
 
   return (
@@ -21,7 +41,7 @@ function App() {
             setUsername={setUsername}
             handleClick={fetchUser}
           />
-          {/* <Card data={data} /> */}
+          <Card data={data} />
         </div>
       </div>
     </section>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,30 @@
 import { CardType } from "../types/cardType";
+import Display from "./Display";
+import NotFound from "./NotFound";
 
 interface CardProps {
   data?: CardType | null;
 }
 
 const Card = ({ data }: CardProps) => {
+  if (!data) return <NotFound />;
+  const { image, username, createdAt, bio, publicRepos, followers, following } =
+    data;
   return (
-    <div>
-      Card
+    <div className="flex flex-col gap-5 p-5 rounded-md bg-primary-100">
+      <div className="flex gap-4">
+        <img src={image} alt="" className="w-24 h-24 rounded-full" />
+        <div>
+          <h2 className="font-semibold">{username}</h2>
+          <h2>Joined-At: {createdAt}</h2>
+        </div>
+      </div>
+      <p>{bio}</p>
+      <Display
+        publicRepos={publicRepos}
+        followers={followers}
+        following={following}
+      />
     </div>
   );
 };
